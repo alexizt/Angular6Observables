875bb3e1412e883475b16a9caef3812cd6e95868
atriemstra <alexis.triemstra@corcoran.com>
2018-09-25 18:54:10 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { UserService } from './user.service';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'app';
+  private users$: Observable<any[]>;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.users$ = this.userService.getUsers();
+    this.userService.loadDummyData();
+  }
+
+  createUser(user) {
+    this.userService.createNewUser(user);
+  }
+
+  approveAll() {
+    this.userService.approveAll();
+  }
 }
